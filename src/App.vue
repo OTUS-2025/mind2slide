f1158cf0b69f87b463d27682d96e61f9410f0513
Max V. Shumakov <shumakovmv@gmail.com>
2025-11-12 11:58:33 +0300

    Show data from JSON-file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import treeData from './moc/jsMind-MRYA-01.json'
+import { Button } from 'primevue'
+const tree = reactive(treeData)
+const toggleBranch = (node) => {
+  node.expanded = !node.expanded
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container mx-auto">
+    <h1>Mind To Slide</h1>
+    <Button label="Take it!" icon="pi pi-user" variant="outlined" raised />
+    <p v-if="tree.meta !== ''">
+      {{ tree.meta.name }} - {{ tree.meta.author }} {{ tree.meta.version }}
+    </p>
+    <p v-else>Meta not set</p>
+    <div v-if="tree.data !== ''" class="">
+      <div class="grid grid-cols-4 gap-4">
+        <div class="">
+          <Button
+            v-if="tree.data.children !== ''"
+            @click="toggleBranch(tree.data)"
+            icon="pi pi-check"
+            rounded
+            variant="outlined"
+            size="small"
+            aria-label="Filter"
+          />
+        </div>
+        <div class="">
+          {{ tree.data.topic }}
+        </div>
+      </div>
+
+      <ol v-show="tree.data.expanded" class="">
+        <li v-for="(item, ndx) in tree.data.children" :key="ndx" class="">
+          {{ item.topic }}
+          <ul v-if="item.children !== ''">
+            <li v-for="(child, ndx) in item.children" :key="ndx" class="">
+              {{ child.topic }}
+            </li>
+          </ul>
+        </li>
+      </ol>
+    </div>
+    <p v-else>Data not set</p>
+  </div>
 </template>
 
 <style scoped></style>
