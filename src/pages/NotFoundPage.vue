f03de0d815b4d35474816a357c934931364c2a28
Max V. Shumakov <shumakovmv@gmail.com>
2025-11-29 06:23:21 +0300

    404 page upd

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-80 w-full flex justify-center content-center">
+  <div class="h-80 w-full flex items-center justify-center">
     <h2 class="text-2xl">У нас такого нет!</h2>
   </div>
 </template>
